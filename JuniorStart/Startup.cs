1b597c74830e228c4ce123846b215b6a47104f0d
Grzesiek <grzegorzociepka09@gmail.com>
2019-07-14 12:17:04 +0200

    jwt configuration added to startup

--- JuniorStart/Startup.cs
+++ JuniorStart/Startup.cs
@@ -1,11 +1,16 @@
-﻿using FluentValidation.AspNetCore;
+﻿using System.Text;
+using FluentValidation.AspNetCore;
 using JuniorStart.Configurations;
 using JuniorStart.Repository;
+using JuniorStart.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Threading.Tasks;
 
 namespace JuniorStart
 {
@@ -33,9 +38,44 @@ namespace JuniorStart
             services.AddTransient<ApplicationSeed>();
 
             services.ConfigureFilters();
+            
+            //jwt
+            byte[] key = Encoding.ASCII.GetBytes(Configuration.GetSection("JWT").GetSection("SecretKey").Value);
+            services.AddAuthentication(x =>
+                {
+                    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(x =>
+                {
+                    x.Events = new JwtBearerEvents
+                    {
+                        OnTokenValidated = context =>
+                        {
+                            var userService = context.HttpContext.RequestServices.GetRequiredService<IUserService>();
+                            var userId = context.Principal.Identity.Name;
+                            var user = userService.GetById(userId);
+                            if (user == null)
+                            {
+                                // return unauthorized if user no longer exists
+                                context.Fail("Unauthorized");
+                            }
+                            return Task.FromResult(0);
+                        }
+                    };
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+                });
 
-//          services.ConfigureAuthentication();
             services.ConfigureSwagger();
+            services.AddScoped<IAuthenticationService, AuthenticationService>();
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ApplicationSeed applicationSeed)
@@ -50,7 +90,7 @@ namespace JuniorStart
             }
 
             app.ExceptionHandler();
-//          app.UseAuthentication();
+            app.UseAuthentication();
 
             app.UseHttpsRedirection();
             app.UseMvc();
